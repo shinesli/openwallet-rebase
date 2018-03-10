51b3719f97ee2f69e11ef049d0baa3abc8c88291
Yudong Li <liyudong1@hotmail.com>
2018-03-10 00:49:57 -0600

    config change

--- html/js/config.js
+++ html/js/config.js
@@ -3,25 +3,25 @@ var config = {
     mainnetExplorerUrl: "https://moneroexplorer.com/",
     testnetExplorerUrl: "http://139.162.32.245:8082/",
     testnet: true,
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 9,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-    txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    coinSymbol: 'XMR',
-    openAliasPrefix: "xmr",
-    coinName: 'Monero',
-    coinUriPrefix: 'monero:',
-    addressPrefix: 18,
-    integratedAddressPrefix: 19,
-    addressPrefixTestnet: 53,
-    integratedAddressPrefixTestnet: 54,
+    txCoinbaseMinConfirms: 20, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
+    coinSymbol: 'ARS',
+    openAliasPrefix: "ars",
+    coinName: 'Aureus',
+    coinUriPrefix: 'aureus:',
+    addressPrefix: 49f4a,
+    integratedAddressPrefix: 1fdd4a,
+    addressPrefixTestnet: 0xc526,
+    integratedAddressPrefixTestnet: 0x3cdd25,
     feePerKB: new JSBigInt('2000000000'),//20^10 - for testnet its not used, as fee is dynamic.
     dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     txChargeRatio: 0.5,
-    defaultMixin: 4, // minimum mixin for hardfork v5
+    defaultMixin: 1, // minimum mixin for hardfork v5
     txChargeAddress: '',
     idleTimeout: 30,
     idleWarningDuration: 20,
     maxBlockNumber: 500000000,
-    avgBlockTime: 120,
+    avgBlockTime: 60,
     debugMode: false
 };
