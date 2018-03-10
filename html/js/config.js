2510a1a5ab8eb3a214f570faa7b7ddb5ad19249f
Yudong Li <liyudong1@hotmail.com>
2018-03-10 10:58:40 -0600

    no testnet

--- html/js/config.js
+++ html/js/config.js
@@ -2,7 +2,7 @@ var config = {
     apiUrl: "http://127.0.0.1:1984/",
     mainnetExplorerUrl: "https://moneroexplorer.com/",
     testnetExplorerUrl: "http://139.162.32.245:8082/",
-    testnet: true,
+    testnet: false,
     coinUnitPlaces: 9,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
     txCoinbaseMinConfirms: 20, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
